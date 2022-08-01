04bb98891f4b1835e97f75ad3d0a63815ec870f6
ve-D-yA <108597415+ve-D-yA@users.noreply.github.com>
2022-08-01 13:19:02 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,118 @@
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image();
+	img_imgTag.onload = uploadimg;
+	img_imgTag.src = img_image;
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		{
+			aplhabetkey();
+			document.getElementById("d1").innerHTML="You pressed on an Alphabet";
+			console.log("alphabet key");
+		}
+	else{
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	}
+}
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=48 && keyPressed<=57))
+		{
+			numberkey();
+			document.getElementById("d2").innerHTML="You pressed on a Number";
+			console.log("number key");
+		}
+	else{
+		otherkey();
+		document.getElementById("d2").innerHTML="You pressed symbol or other key";
+	}
+}
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=37 && keyPressed<=40))
+		{
+			arrowkey();
+			document.getElementById("d3").innerHTML="You pressed on an Arrow Key";
+			console.log("alphabet key");
+		}
+	else{
+		otherkey();
+		document.getElementById("d3").innerHTML="You pressed symbol or other key";
+	}
+}
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=17 && keyPressed<=27))
+		{
+			specialkey();
+			document.getElementById("d4").innerHTML="You pressed on a Special Character";
+			console.log("special key");
+		}
+	else{
+		otherkey();
+		document.getElementById("d4").innerHTML="You pressed symbol or other key";
+	}
+}
+
+function aplhabetkey()
+{
+	img_image="Alpkey.png";
+	add(); 
+}
+function numberkey()
+{
+	img_image="numkey.png";
+	add();
+}
+function arrowkey()
+{
+	img_image="Arrkey.png";
+	add();
+}
+function specialkey()
+{
+	img_image="spkey.png";
+	add();
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
